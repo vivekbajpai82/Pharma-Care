74c30f94af82b2a9c5c9180f6d77954e061a0964
vivekbajpai82 <bajpaivivek82@gmail.com>
2025-09-30 14:01:54 +0530

    Fix: emailserverproblem

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const createTransporter = () => {
     return nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
